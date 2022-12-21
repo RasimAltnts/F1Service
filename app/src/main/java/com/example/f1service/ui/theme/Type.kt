1e63c6b565f629eaffd836e31d9969eaf73764ad
Rasim ALTUNTAŞ <rasim.altuntas@amperino.com>
2022-12-21 09:32:28 +0300

    Data Models Moved in F1 Folders

--- app/src/main/java/com/example/f1service/ui/theme/Type.kt
+++ app/src/main/java/com/example/f1service/ui/theme/Type.kt
@@ -2,33 +2,36 @@ package com.example.f1service.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.f1service.R
+
+//Custom Font Family
+val customFontFamily = FontFamily(
+    Font(R.font.kumbhsans_light, weight = FontWeight.Light),
+    Font(R.font.kumbhsans_regular, weight = FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = customFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+    bodyMedium = TextStyle(
+        fontFamily = customFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+
     )
-    */
+    /**
+     * Custom Typography
+     */
 )
